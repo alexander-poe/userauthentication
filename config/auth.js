2b7edfa3d05e9fcef72309f10221bc89c215059f
alexpoe22 <alexpoe22@gmail.com>
2016-12-28 20:37:14 -0800

    facebook auth working *email field undefined?

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1526909184004196', // your App ID
+        'clientSecret'  : 'c92569262ed692099116f6736f27b2a2', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
+
